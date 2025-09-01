d890fbf966c6f43c8579cc7f6b556778aa23ccc7
schoolop <blookethacks92@gmail.com>
2025-09-01 16:29:19 -0500

    Update server.js

--- server.js
+++ server.js
@@ -62,7 +62,9 @@ async function hasManagerRole(userId) {
 
 // Routes
 app.get("/", (req, res) => res.sendFile(path.join(__dirname, "views/index.html")));
+app.get("/info", (req, res) => res.sendFile(path.join(__dirname, "views/info.html"))); // Added this route
 app.get("/status", (req, res) => res.sendFile(path.join(__dirname, "views/status.html")));
+
 
 // Discord OAuth login
 app.get("/login", passport.authenticate("discord"));
